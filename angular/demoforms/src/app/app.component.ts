3dad4a887e0a655b194d258f4297bd4c0dd23f1f
Júlio Machado <julioapm@gmail.com>
2024-07-22 20:53:56 -0300

    Atualização demo forms template-driven

--- angular/demoforms/src/app/app.component.ts
+++ angular/demoforms/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CadastroReactiveComponent } from './cadastro-reactive/cadastro-reactive.component';
+import { CadastroTemplateDrivenComponent } from "./cadastro-template-driven/cadastro-template-driven.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CadastroReactiveComponent],
+  imports: [RouterOutlet, CadastroReactiveComponent, CadastroTemplateDrivenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular/demoforms/src/app/cadastro-template-driven/cadastro-template-driven.component.ts
+++ angular/demoforms/src/app/cadastro-template-driven/cadastro-template-driven.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Pessoa } from '../models/pessoa.model';
 
 @Component({
   selector: 'app-cadastro-template-driven',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cadastro-template-driven.component.html',
   styleUrl: './cadastro-template-driven.component.css'
 })
 export class CadastroTemplateDrivenComponent {
+  pessoa = new Pessoa('Anônima', 0);
 
+  onSubmit() {
+    console.log(this.pessoa);
+  }
 }
